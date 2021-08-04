abba3487015ed03501e10a9144855ffdcd57726a
VinyVidal <vinicius.vidalp@hotmail.com>
2021-08-04 10:28:29 -0300

    Fixing Models classname

--- app/Models/ProjectLink.php
+++ app/Models/ProjectLink.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Project extends Model
+class ProjectLink extends Model
 {
     // use HasFactory;
     use SoftDeletes;
