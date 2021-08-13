36e192cd1c4086a5d163b3717fda5e88a821efe2
VinyVidal <vinicius.vidalp@hotmail.com>
2021-08-13 13:11:44 -0300

    Sanctum package setup

--- app/Models/ApiUser.php
+++ app/Models/ApiUser.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class ApiUser extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -29,8 +29,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password'
     ];
 
     /**
